82b58f5b621042350270c61b58941cf6156bcccb
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-07-08 20:39:20 +0530

    added error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Error() {
   return (
-    <div>Error</div>
-  )
+    <div className="flex justify-center align-middle text-center pt-[25%]  p-5">
+      <div className="">
+        <h1 className="text-4xl font-bold">Error-404 !! Page Not Found</h1>
+      </div>
+    </div>
+  );
 }
